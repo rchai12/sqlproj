838f3924960bdc22a2c3af21804d742a32b1bd08
rchai12 <digital24688@live.com>
2025-03-05 21:51:01 -0500

    got folders to show, can't get the cards to show

--- lib/DatabaseHelper.dart
+++ lib/DatabaseHelper.dart
@@ -3,14 +3,24 @@ import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart';
 
 class DatabaseHelper {
-  static const _databaseName = "MyDatabase.db";
+  static const _databaseName = "CardsDatabase.db";
   static const _databaseVersion = 1;
-  static const table = 'my_table';
-  static const columnId = '_id';
+
+  // Table and column names
+  static const foldersTable = 'folders';
+  static const cardsTable = 'cards';
+
+  static const columnId = 'id';
   static const columnName = 'name';
-  static const columnAge = 'age';
+  static const columnTimestamp = 'timestamp';
+  static const columnSuit = 'suit';
+  static const columnImageUrl = 'imageUrl';
+  static const columnFolderId = 'folderId';
+  static const columnFolderImageUrl = 'folderImageUrl';
+
   late Database _db;
-// this opens the database (and creates it if it doesn't exist)
+
+  // Initializes database and prepopulates data
   Future<void> init() async {
     final documentsDirectory = await getApplicationDocumentsDirectory();
     final path = join(documentsDirectory.path, _databaseName);
@@ -19,73 +29,187 @@ class DatabaseHelper {
       version: _databaseVersion,
       onCreate: _onCreate,
     );
+    await _prepopulateData();
   }
 
-// SQL code to create the database table
+  // Create tables if not exists
   Future _onCreate(Database db, int version) async {
-    await db.execute('''
-CREATE TABLE $table (
-$columnId INTEGER PRIMARY KEY,
-$columnName TEXT NOT NULL,
-$columnAge INTEGER NOT NULL
-)
-''');
+    await db.execute('''CREATE TABLE $foldersTable (
+        $columnId INTEGER PRIMARY KEY AUTOINCREMENT,
+        $columnName TEXT NOT NULL,
+        $columnTimestamp TEXT DEFAULT CURRENT_TIMESTAMP,
+        $columnFolderImageUrl TEXT NOT NULL
+    )''');
+
+    await db.execute('''CREATE TABLE $cardsTable (
+        $columnId INTEGER PRIMARY KEY AUTOINCREMENT,
+        $columnName TEXT NOT NULL,
+        $columnSuit TEXT NOT NULL,
+        $columnImageUrl TEXT NOT NULL,
+        $columnFolderId INTEGER,
+        FOREIGN KEY ($columnFolderId) REFERENCES $foldersTable($columnId)
+    )''');
   }
 
-// Helper methods
-// Inserts a row in the database where each key in the
-//Map is a column name
-// and the value is the column value. The return value
-//is the id of the
-// inserted row.
-  Future<int> insert(Map<String, dynamic> row) async {
-    return await _db.insert(table, row);
+  // Prepopulate folders and cards data
+  Future<void> _prepopulateData() async {
+    final suits = ['Diamonds', 'Clubs', 'Hearts', 'Spades'];
+    final folderImages = [
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/SuitDiamonds.svg/800px-SuitDiamonds.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/SuitClubs.svg/800px-SuitClubs.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/a/a0/Naipe_copas.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/SuitSpades.svg/800px-SuitSpades.svg.png'
+    ];
+    final cardImages = [
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/01_of_diamonds_A.svg/800px-01_of_diamonds_A.svg.png', 
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/02_of_diamonds.svg/800px-02_of_diamonds.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/03_of_diamonds.svg/800px-03_of_diamonds.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/04_of_diamonds.svg/800px-04_of_diamonds.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/05_of_diamonds.svg/800px-05_of_diamonds.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/06_of_diamonds.svg/800px-06_of_diamonds.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/07_of_diamonds.svg/800px-07_of_diamonds.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/08_of_diamonds.svg/800px-08_of_diamonds.svg.png', 
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/09_of_diamonds.svg/800px-09_of_diamonds.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/10_of_diamonds_-_David_Bellot.svg/800px-10_of_diamonds_-_David_Bellot.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Jack_of_diamonds_fr.svg/800px-Jack_of_diamonds_fr.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/Queen_of_diamonds_fr.svg/800px-Queen_of_diamonds_fr.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/King_of_diamonds_fr.svg/800px-King_of_diamonds_fr.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/01_of_clubs_A.svg/800px-01_of_clubs_A.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/02_of_clubs.svg/800px-02_of_clubs.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/03_of_clubs.svg/800px-03_of_clubs.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/04_of_clubs.svg/800px-04_of_clubs.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/05_of_clubs.svg/800px-05_of_clubs.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/06_of_clubs.svg/800px-06_of_clubs.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/07_of_clubs.svg/800px-07_of_clubs.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/08_of_clubs.svg/800px-08_of_clubs.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/09_of_clubs.svg/800px-09_of_clubs.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/10_of_clubs_-_David_Bellot.svg/800px-10_of_clubs_-_David_Bellot.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Jack_of_clubs_fr.svg/800px-Jack_of_clubs_fr.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Queen_of_clubs_fr.svg/800px-Queen_of_clubs_fr.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/King_of_clubs_fr.svg/800px-King_of_clubs_fr.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/01_of_hearts_A.svg/800px-01_of_hearts_A.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/02_of_hearts.svg/800px-02_of_hearts.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/03_of_hearts.svg/800px-03_of_hearts.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/04_of_hearts.svg/800px-04_of_hearts.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/05_of_hearts.svg/800px-05_of_hearts.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/06_of_hearts.svg/800px-06_of_hearts.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/07_of_hearts.svg/800px-07_of_hearts.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/08_of_hearts.svg/800px-08_of_hearts.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/09_of_hearts.svg/800px-09_of_hearts.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/10_of_hearts_-_David_Bellot.svg/800px-10_of_hearts_-_David_Bellot.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/Jack_of_hearts_fr.svg/800px-Jack_of_hearts_fr.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Queen_of_hearts_fr.svg/800px-Queen_of_hearts_fr.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/King_of_hearts_fr.svg/800px-King_of_hearts_fr.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/01_of_spades_A.svg/800px-01_of_spades_A.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/02_of_spades.svg/800px-02_of_spades.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/03_of_spades.svg/800px-03_of_spades.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/04_of_spades.svg/800px-04_of_spades.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/05_of_spades.svg/800px-05_of_spades.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/06_of_spades.svg/800px-06_of_spades.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/07_of_spades.svg/800px-07_of_spades.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/08_of_spades.svg/800px-08_of_spades.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/09_of_spades.svg/800px-09_of_spades.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/10_of_spades_-_David_Bellot.svg/800px-10_of_spades_-_David_Bellot.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Jack_of_spades_fr.svg/800px-Jack_of_spades_fr.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Queen_of_spades_fr.svg/800px-Queen_of_spades_fr.svg.png',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/King_of_spades_fr.svg/800px-King_of_spades_fr.svg.png'
+    ];
+
+    deleteAllCards();
+    deleteAllFolders();
+    int imageIndex = 0;
+    List<Map<String, dynamic>> folders = await queryAllRows();
+    if (folders.isEmpty) {
+      for (var i = 0; i < suits.length; i++) {
+        String suit = suits[i];
+        String folderImageUrl = folderImages[i];
+        int folderId = await insertFolder(suit, folderImageUrl);
+
+        print('Inserted folder: $suit with id $folderId');
+        // Insert 13 cards for each suit
+        for (var rank = 1; rank <= 13; rank++) {
+          String cardName = _getCardName(rank);
+          await insertCard({
+            columnName: '$cardName of $suit',
+            columnSuit: suit,
+            columnImageUrl: cardImages[imageIndex],
+            columnFolderId: folderId,
+          });
+          print('Inserted card: $cardName with id $suit');
+          imageIndex++;
+        }
+      }
+    }
   }
 
-// All of the rows are returned as a list of maps, where each map is
-// a key-value list of columns.
+  // Helper function to convert rank to card name
+  String _getCardName(int rank) {
+    switch (rank) {
+      case 1:
+        return 'Ace';
+      case 11:
+        return 'Jack';
+      case 12:
+        return 'Queen';
+      case 13:
+        return 'King';
+      default:
+        return rank.toString();
+    }
+  }
+
+  // Insert folder into the database
+  Future<int> insertFolder(String name, String imageUrl) async {
+    return await _db.insert(foldersTable, {
+      columnName: name,
+      columnFolderImageUrl: imageUrl
+    });
+  }
+
+  // Insert card into the database
+  Future<int> insertCard(Map<String, dynamic> row) async {
+    return await _db.insert(cardsTable, row);
+  }
+
+  // Query all folders
   Future<List<Map<String, dynamic>>> queryAllRows() async {
-    return await _db.query(table);
+    return await _db.query(foldersTable);
   }
 
-// All of the methods (insert, query, update, delete) can also be done using
-// raw SQL commands. This method uses a raw query to give the row count.
-  Future<int> queryRowCount() async {
-    final results = await _db.rawQuery('SELECT COUNT(*) FROM $table');
-    return Sqflite.firstIntValue(results) ?? 0;
-  }
-
-// We are assuming here that the id column in the map is set. The other
-// column values will be used to update the row.
-  Future<int> update(Map<String, dynamic> row) async {
-    int id = row[columnId];
-    return await _db.update(
-      table,
-      row,
-      where: '$columnId = ?',
-      whereArgs: [id],
+  // Query cards for a specific folder
+  Future<List<Map<String, dynamic>>> query(int folderId) async {
+    return await _db.query(
+      cardsTable,
+      where: '$columnFolderId = ?',
+      whereArgs: [folderId],
     );
   }
 
-// Deletes the row specified by the id. The number of affected rows is
-// returned. This should be 1 as long as the row exists.
+  // Delete card by id
   Future<int> delete(int id) async {
     return await _db.delete(
-      table,
+      cardsTable,
       where: '$columnId = ?',
       whereArgs: [id],
     );
   }
 
-  Future<List<Map<String, dynamic>>> query(int id) async {
-    return await _db.query(
-      table,
-      where: '$columnId = ?',
-      whereArgs: [id],
-    );
+  // Query all cards
+  Future<List<Map<String, dynamic>>> queryAllCards() async {
+    return await _db.query(cardsTable);
   }
 
-  Future<int> deleteAllRecords() async {
-    return await _db.delete(table);
+  // Close the database connection
+  Future<void> close() async {
+    await _db.close();
   }
+
+  Future<int> deleteAllFolders() async {
+    return await _db.delete(foldersTable);
+  }
+
+  Future<int> deleteAllCards() async {
+    return await _db.delete(cardsTable);
+  }
+  
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,123 +1,119 @@
 import 'package:flutter/material.dart';
 import 'DatabaseHelper.dart';
 
-void main() {
-  runApp(const MyApp());
+
+final _databaseHelper = DatabaseHelper();
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await _databaseHelper.init();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Card Folders',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: FoldersScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class FoldersScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _FoldersScreenState createState() => _FoldersScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _FoldersScreenState extends State<FoldersScreen> {
+  List<Map<String, dynamic>> _folders = [];
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    _loadFolders();
+  }
+
+  // Load the folders from the database
+  Future<void> _loadFolders() async {
+    List<Map<String, dynamic>> folders = await _databaseHelper.queryAllRows();
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _folders = folders;
     });
+    // Debug: Print the folders to verify
+    print("Folders Loaded: $_folders");
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      appBar: AppBar(title: Text('Folders')),
+      body: _folders.isEmpty
+          ? Center(child: Text('No folders available'))
+          : ListView.builder(
+              itemCount: _folders.length,
+              itemBuilder: (context, index) {
+                var folder = _folders[index];
+                return ListTile(
+                  title: Text(folder['name']),
+                  leading: Image.network(folder['folderImageUrl']),
+                  onTap: () {
+                    // Navigate to the CardsScreen with the folder data
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => CardsScreen(folderId: folder['id']),
+                      ),
+                    );
+                  },
+                );
+              },
             ),
-          ],
-        ),
+    );
+  }
+}
+
+class CardsScreen extends StatelessWidget {
+  final int folderId;
+  CardsScreen({required this.folderId});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Cards in Folder')),
+      body: FutureBuilder<List<Map<String, dynamic>>>(
+        future: DatabaseHelper().query(folderId),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(child: Text('No cards available.'));
+          } else {
+            var cards = snapshot.data!;
+            return GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+              ),
+              itemCount: cards.length,
+              itemBuilder: (context, index) {
+                var card = cards[index];
+                return Card(
+                  child: Column(
+                    children: [
+                      Image.network(card['imageUrl']),
+                      Text(card['name']),
+                    ],
+                  ),
+                );
+              },
+            );
+          }
+        },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
